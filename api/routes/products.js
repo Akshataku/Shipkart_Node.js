c9074fbe2754e8f44296a0e3bf87b231555a6d33
Akshat <akshat2002garg@gmail.com>
2023-09-02 21:57:35 +0530

    API created and linked with DB.

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,44 +1,102 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get('/',(req, res, next) => {
-    res.status(200).json({
-        message:'Handling GET requests to /products'
+    Product.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
+
 router.post('/',(req, res, next) => {
-    res.status(201).json({
-        message: 'Handling POST requests to /products'
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        item: req.body.item,
+        price: req.body.price,
+        brand: req.body.brand
+    });
+    product
+    .save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Handling POST requests to /products',
+            createdProduct: result
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
+
 
 
 router.get('/:productId',(req, res, next) => {
-    const Id = req.params.productId;
-    if(Id === 'special'){
-        res.status(200).json({
-            message: 'You discovered special Id',
-            id:Id
-        });
-    }
-    else{
-        res.status(200).json({
-            message: 'You passed an Id',
-            id: Id
-        });
-    }
+    const id = req.params.productId;
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+        console.log("From Database",doc);
+        if(doc){
+            res.status(200).json(doc);
+        }else{
+            res.status(404).json({
+                message: 'No valid entry found with that id'
+            });
+        }
+       
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error:err});
+    });
 });
 
 router.patch('/:productId',(req, res, next) => {
-    res.status(200).json({
-        message: 'Updated Product'
+    const id = req.params.productId;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({_id:id},{ $set: updateOps})
+    .exec()
+    .then(result => {
+        console.log()
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     });
 });
 
 router.delete('/:productId',(req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted Product'
+    const id = req.params.productId;
+    Product.deleteOne({_id:id})
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     });
 });
 
--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const url = "mongodb://127.0.0.1:27017/shipkart"
 
-// app.use('/',(req, res, next) => {
-//     res.status(200).json({
-//         message : 'Akshat is in his zone.'
-//     });
-// });
 
 const productRoutes = require('./api/routes/products');
+const customerRoutes = require('./api/routes/customers');
 const orderRoutes = require('./api/routes/orders');
 
+// mongoose.connect('mongodb+srv://akshat_garg_07:'+ process.env.MONGO_ATLAS_PW +'@cluster0.acgi0kx.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(url,{})
+.then(result=>console.log("database connected"))
+.catch(err=>console.log(err))
+
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+//hnadling CORS error 
+//cross origin resource sharing
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods',
+        'PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({})
+    }
+    next();
+});
 
 //routes which should handle requests
 app.use('/products',productRoutes);
+app.use('/customers',customerRoutes);
 app.use('/orders',orderRoutes);
 
 //when upper 2 routes are not able to handle
