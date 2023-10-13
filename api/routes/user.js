45b6ce37af30982db86f550c93b94fbbf486c48b
Akshat <akshat2002garg@gmail.com>
2023-10-13 14:44:20 +0530

    Login Api

--- api/routes/user.js
+++ api/routes/user.js
@@ -8,6 +8,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+//for google auth sign up
 router.post('/', (req, res, next) => {
     User.find({email:req.body.email})
     .exec()
@@ -18,7 +19,6 @@ router.post('/', (req, res, next) => {
             });
         }
         else{
-            
             const user = new User({
                 _id: new mongoose.Types.ObjectId(),
                 firstname: req.body.firstName,
@@ -38,53 +38,83 @@ router.post('/', (req, res, next) => {
                     error: err
                 });
             });
-
         }
     })
 });
 
-// router.post('/signup', (req, res, next) => {
-//     User.find({email:req.body.email})
-//     .exec()
-//     .then(user => {
-//         if(user.length > 0){
-//             return res.status(409).json({
-//                 message:'E-mail already exists'
-//             });
-//         } 
-//         else{
-//             bcrypt.hash(req.body.password,10, (err, hash) => {
-//                 if(err){
-//                     return res.status(500).json({
-//                         error: err
-//                     });
-//                 } else{
-//                     const user = new User({
-//                         _id: new mongoose.Types.ObjectId(),
-//                         firstname: req.body.firstname,
-//                         lastname: req.body.lastname,
-//                         email: req.body.email,
-//                         password: hash
-//                     });
-//                     user.save()
-//                     .then(result =>{
-//                         console.log(result);
-//                         res.status(201).json({
-//                             message: 'User Created'
-//                         });
-//                     })
-//                     .catch(err => {
-//                         console.log(err);
-//                         res.status(500).json({
-//                             error: err
-//                         });
-//                     });
-//                 }
-//             });
-//         }
-//     });
-// });
+//manually sign up
+router.post('/signup', (req, res, next) => {
+    User.find({email:req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length > 0){
+            return res.status(409).json({
+                message:'E-mail already exists'
+            });
+        } 
+        else{
+            bcrypt.hash(req.body.password,10, (err, hash) => {
+                if(err){
+                    return res.status(500).json({
+                        error: err
+                    });
+                } else{
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user.save()
+                    .then(result =>{
+                        console.log(result);
+                        res.status(201).json({
+                            message: 'User Created'
+                        });
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({
+                            error: err
+                        });
+                    });
+                }
+            });
+        }
+    });
+});
 
+//google login
+router.post('/googlelogin', (req, res, next) => {
+    User.find({email: req.body.email})
+    .exec()
+    .then(user => {
+        //array of users 
+        if(user.length < 1){
+            return res.status(401).json({
+                message: 'Auth Failed! User doesnt exists'
+            });
+        } else{           
+                
+                //json web token
+            const token = jwt.sign({email: user[0].email, userId: user[0]._id }, process.env.JWT_KEY, {expiresIn:"1h"});
+
+            return res.status(200).json({
+                message: 'Auth Successful',
+                token: token
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+//manually login
 router.post('/login', (req, res, next) => {
     User.find({email: req.body.email})
     .exec()
@@ -92,13 +122,13 @@ router.post('/login', (req, res, next) => {
         //array of users 
         if(user.length < 1){
             return res.status(401).json({
-                message: 'Auth Failed'
+                message: 'Auth Failed! User doesnt exists'
             });
         } else{            //user texted      //hashed password when sign in
             bcrypt.compare(req.body.password, user[0].password, (err,result) => {
                 if(err){
                     return res.status(401).json({
-                        message: 'Auth Failed'
+                        message: 'Auth Failed!'
                     });
                 }
                 if(result){
@@ -111,7 +141,7 @@ router.post('/login', (req, res, next) => {
                     });
                 }
                 return res.status(401).json({
-                    message: 'Auth Failed'
+                    message: 'Auth Failed! Incorrect Password'
                 });
             })
         }
